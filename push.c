cfb558c65a8f46ee20f7999475a5358e9b2b22c7
Khalid Slik <kslik@c1r6p2.1337.ma>
2022-12-22 17:22:35 +0100

    dd

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: kslik <kslik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 12:09:56 by kslik             #+#    #+#             */
-/*   Updated: 2022/12/17 19:33:37 by kslik            ###   ########.fr       */
+/*   Updated: 2022/12/22 16:06:05 by kslik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 void printa(int *stack, int l, int pbn, int pan)
 {
     int m = l - pbn - 1 + pan;
-    int i = 0;
-    while(i <= m)
+    while(m >= 0)
     {
-        ft_printf("%d\n",stack[i]);
-        i++;
+        ft_printf("%d\n",stack[m]);
+        m--;
     }
     ft_printf("\na\n");
 }
@@ -52,16 +51,12 @@ void rb(int *stackb, int pbn)
 void pb(int *stack,int *stackb, int l, int *pbn)
 {
     ft_printf("pb\n");
-
-    if(!stack[0])
-        exit(0);
-    static int m = 1;
-    int topa = l - m;
+    static int m = 0;
     static int topb = 0;
-    stackb[topb] = stack[topa];
-    stack[topa] = '\0';
-    m++;
+    stackb[topb] = stack[m];
+    stack[m] = '\0';
     topb++;
+    m++;
     (*pbn)++;
 }
 void ra(int *stack, int l,int pbn)
@@ -69,10 +64,10 @@ void ra(int *stack, int l,int pbn)
     ft_printf("ra\n");
 
     int i;
-    i = 0;
+    i = pbn;
     int m;
-    m = l - pbn;
-    stack[m] = stack[0];
+    m = l;
+    stack[m] = stack[pbn];
     while(i < m)
     {
         stack[i] = stack[i + 1];
@@ -102,17 +97,17 @@ void rra(int *stack, int l, int pbn)
     ft_printf("rra\n");
 
     int m ;
-    int i = l - pbn;
+    int i = l - 1;
     int tmp[0];
-    tmp[0] = stack[i - 1];
-    m = i - 1;
-    while(m > 0)
+    tmp[0] = stack[i];
+    m = i ;
+    while(m > pbn)
     {
         stack[m] = stack[m - 1];
         m--;
     }
-    stack[0] = tmp[0];
-    stack[i] = '\0';
+    stack[pbn] = tmp[0];
+    stack[i + 1] = '\0';
 }
 void rrr(int *stack, int *stackb, int l, int pbn)
 {
@@ -128,18 +123,13 @@ void rr(int *stack, int l, int pbn, int *stackb)
 void pa(int *stack,int *stackb, int pbn, int l, int *pan)
 {
     ft_printf("pa\n");
-
-    if(!stack[0] || !stackb[0])
-        exit(0);
     static int m = 1;
     int topb = pbn - m;
-    static int b = 0;
-    int topa = l - pbn - b;
-    stack[topa] = stackb[topb];
+    static int b = 1;
+    stack[b] = stackb[topb];
     stackb[topb] = '\0';
     m++;
     b--;
-
     (*pan)++;
 }
 int sb1(int *stackb, int l)
@@ -173,7 +163,7 @@ int sb(int *stackb, int l)
     }
     return(m);
 }
-void sa(int *stack, int l)
+void sa(int *stack, int l, int pbn)
 {
    
     ft_printf("sa\n");
@@ -181,10 +171,26 @@ void sa(int *stack, int l)
     int tmp[1];
     if(l >= 1)
     {
-        tmp[0] = stack[0];
-        stack[0] = stack[1];
-        stack[1] = tmp[0];
+        tmp[0] = stack[pbn];
+        stack[pbn] = stack[pbn + 1];
+        stack[pbn + 1] = tmp[0];
     }
+}
+int mn(int *stack, int l, int m)
+{
+    int i = m;
+    int in = 1000;
+    int re = 0;
+    while(i < l)
+    {
+        if(stack[i] < in)
+        {
+            in = stack[i];
+            re = i;
+        }
+        i++;
+    }
+    return(re);
 }
 void sa1(int *stack, int l, int m)
 {
@@ -217,6 +223,154 @@ void ss(int *stack, int *stackb, int l)
     sa1(stack,l, m);
     
 }
+void case_of_3(int *stack, int l)
+{
+    if(stack[0] < stack[1] && stack[1] < stack[2] && stack[2] > stack[0])
+        return;
+    else if(stack[0] > stack[1] && stack[2] > stack[0])
+        sa(stack, l,0);
+    else if(stack[0] > stack[1] && stack[1] > stack[2])
+    {    
+        sa(stack, l,0);
+        rra(stack,l, 0);
+    }
+    else if(stack[0] > stack[1] && stack[1] < stack[2])
+        ra(stack, l, 0);
+    else if(stack[0] < stack[1] && stack[2] > stack[0])
+    {
+        sa(stack, l,0);
+        ra(stack, l, 0);
+    }
+    else if(stack[0] < stack[1] && stack[1] > stack[2]) 
+        rra(stack, l, 0);
+}
+void min_to_top(int *stack, int l, int pbn, int m)
+{
+    int min = mn(stack, l, m);
+    if(min == m + 0)
+        return;
+    if(min == m + 1)
+       ra(stack,l, pbn);
+    if(min == m + 2)
+    {
+        ra(stack, l, pbn);
+        ra(stack, l, pbn);
+    }
+    if(min == m + 3)
+        rra(stack, l, pbn);
+}
+void case_of_4(int *stack, int *stackb, int l, int m)
+{
+    int pan = m;
+    int pbn = m;
+    min_to_top(stack, l, pbn, m);
+
+    pb(stack, stackb, l, &pbn);
+    // ft_printf("%d %d %d %d %d\n", stack[0], stack[1], stack[2], stack[3], stack[4]);
+
+    if(stack[m + 1] < stack[m + 2] && stack[m + 2] < stack[m+3] && stack[m + 3] > stack[m + 1])
+                    ;
+    else if(stack[m + 1] > stack[m + 2] && stack[m + 3] > stack[m + 1])
+        sa(stack, l, pbn);
+    else if(stack[m + 1] > stack[m + 2] && stack[m + 2] > stack[m + 3])
+    {    
+        sa(stack, l, pbn);
+        rra(stack,l, m);
+    }
+    else if(stack[m + 1] > stack[m + 2] && stack[m + 2] < stack[m + 1])
+        ra(stack, l, m);
+    else if(stack[m + 1] < stack[m + 2] && stack[m + 3] > stack[m + 1])
+    {
+        sa(stack, l,m);
+        ra(stack, l, m);
+    }
+    else if(stack[m + 1] < stack[m + 2] && stack[m + 2] > stack[m +3]) 
+        rra(stack, l, m);
+    // ft_printf("{%d %d}", stackb[0], stackb[1]);
+
+    pa(stack, stackb, pbn,l, &pan);
+}
+void min_to_top5(int *stack, int l, int pbn)
+{
+    int min= mn(stack, l, 0);
+    if(min == 0)
+        return;
+    if(min == 1)
+        ra(stack, l, 0);
+    if(min == 2)
+    {
+        ra(stack, l, 0);
+        ra(stack, l, 0);
+    }
+    if(min == 3)
+    {
+        rra(stack, l, pbn);
+        rra(stack, l, pbn);
+    }
+    if(min == 4)
+        rra(stack, l, pbn);
+}
+void case_of_5(int *stack, int *stackb, int l)
+{
+    int pan = 0;
+    int pbn = 0;
+    min_to_top5(stack, l, pbn);
+    // ft_printf("%d %d %d %d %d \n", stack[0], stack[1], stack[2], stack[3], stack[4]);
+
+    pb(stack, stackb, l, &pbn);
+    pbn++;
+    if(quit(stack, pbn, l) != 1)
+        case_of_4(stack, stackb,l, 1);
+    pan++;
+    pa(stack, stack, pbn, l, &pan);
+    // ft_printf("%d %d %d %d %d \n", stack[0], stack[1], stack[2], stack[3], stack[4]);
+
+}
+void case_of_2(int *stack, int l)
+{
+    if(stack[0] > stack[1])
+        sa(stack, l, 0);
+}
+int quit(int *stack, int pbn, int l)
+{
+    int i = pbn;
+    while(i < l)
+    {
+        if(stack[i] < stack[i - 1])
+            return(0);
+        i++;  
+    }
+    return(1);
+}
+void finish(int *stack, int *stackb , int l)
+{
+    int pbn = 0;
+    int pan = 0;
+    int min;
+    int total;
+    while(quit(stack, pbn, l) != 1) 
+    {
+        // ft_printf("{%d}", pbn);
+        total = l - pbn;
+        // ft_printf("total = %d", total / 2);
+
+        min = mn(stack, l, pbn);
+        // ft_printf("min = %d", min);
+
+        if(min == pbn)
+            pb(stack, stackb, l, &pbn);
+        else if(min >= total / 2 )
+            rra(stack, l, pbn);
+        else if(min < total / 2)
+            ra(stack,l, pbn);
+    }
+    total = pbn;
+    while(total > 0)
+    {
+        pa(stack, stackb, pbn, l, &pan);
+        total--;
+    }
+}
 void va(int l, int *arr)
 {
     int stack[l];
@@ -228,20 +382,38 @@ void va(int l, int *arr)
         count++;
     }
     //----------------//
-
     // ra(stack, l, pbn);
-    // pb(stack, stackb, l, &pbn);
+    // sa(stack, l);
     // pb(stack, stackb, l, &pbn);
     // pb(stack, stackb, l, &pbn);
     // pb(stack, stackb, l, &pbn);
     // pushswap(stack, stackb, l, pbn);
     // rrr(stack, stackb, l, pbn);
-    sort(stack, stackb, l);
+    // if(l == 2)
+    //     case_of_2(stack, l);
+    // if(l == 3)
+    //     case_of_3(stack, l);
+    // if(l == 4)
+    //     case_of_4(stack,stackb, l);
+    int pan = 0;
+    int pbn = 0;
+
+    // case_of_3(stack,l);
+    // if(l == 2)
+    //     case_of_2(stack, l);
+    // if(l == 3)
+    //     case_of_3(stack, l);
+    // if(l == 4)
+    //     case_of_4(stack, stackb, l, 0);
+    // if(l == 5)
+    //     case_of_5(stack,stackb, l);
+    finish(stack, stackb,l);
+    // ft_printf("%d",quit(stack, 1, l));
     // sort_check(stack, l);
     // ft_printf("%d",min_nbr(stack, l));
     // pb(stack, stackb, l, &pan);
-    // printa(stack, l, pbn, pan);
     // printb(stackb, pbn, pan);
+
     //----------------//
 }
 #include <string.h>
--- push.h
+++ push.h
@@ -6,7 +6,7 @@
 /*   By: kslik <kslik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 12:09:54 by kslik             #+#    #+#             */
-/*   Updated: 2022/12/17 19:28:27 by kslik            ###   ########.fr       */
+/*   Updated: 2022/12/22 15:06:03 by kslik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 #include "ft_printf.h"
 #include <stdlib.h>
 #include <stdlib.h>
-int *sort_check(int *stack, int l);
+void finish(int *stack, int *stackb , int l);
+void min_to_top5(int *stack, int l, int pbn);
+int mn(int *stack, int l, int m);
+int quit(int *stack, int pbn,int l);
+void case_of_5(int *stack, int *stackb, int l);
+void case_of_2(int *stack, int l);
+void case_of_4(int *stack, int *stackb, int l, int m);
+void min_to_top(int *stack, int l, int pbn, int m);
+void case_of_3(int *stack, int l);
 int min_nbr(int *stack, int pbn);
-void sort(int *stack, int *stackb, int l);
 void printa(int *stack, int l, int pbn, int pan);
 void printb(int *stack,int pbn, int pan);
 void rb(int *stackb, int pbn);
@@ -30,11 +37,10 @@ void rr(int *stack, int l, int pbn, int *stackb);
 void pa(int *stack,int *stackb, int pbn, int l, int *pan);
 int sb1(int *stackb, int l);
 int sb(int *stackb, int l);
-void sa(int *stack, int l);
+void sa(int *stack, int l, int pbn);
 void sa1(int *stack, int l, int m);
 void ss(int *stack, int *stackb, int l);
 void va(int l, int *arr);
-void pushswap(int *stack, int *stackb, int l, int pbn);
-
+int check(int *stack, int l);
 
 #endif
